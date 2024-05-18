f106e117fb953faa0a8f84cdd684426cd81dc900
sadikatanisha <sadikatanisha@gmail.com>
2024-05-18 11:38:06 +0600

    fix:data

--- frontend/redux/store.ts
+++ frontend/redux/store.ts
@@ -15,9 +15,9 @@ export const store = configureStore({
 
 //call refresh token on every page load
 const initializeApp = async () => {
-  // await store.dispatch(
-  //   apiSlice.endpoints.refreshToken.initiate({}, { forceRefetch: true })
-  // );
+  await store.dispatch(
+    apiSlice.endpoints.refreshToken.initiate({}, { forceRefetch: true })
+  );
   await store.dispatch(
     apiSlice.endpoints.loadUser.initiate({}, { forceRefetch: true })
   );
